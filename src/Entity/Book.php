4107a17bc86e8d48c5546d8c26a9ba504f19bebc
Loïc Frémont <lc.fremont@gmail.com>
2022-07-05 15:08:58 +0200

    Init project

---
+++ src/Entity/Book.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Resource\Annotation\SyliusCrudRoutes;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[SyliusCrudRoutes(
+    alias: 'app.book',
+    path: 'admin/semantic_ui/books',
+    section: 'semantic_ui',
+    redirect: 'update',
+    templates: '@SyliusUxSemanticUi/crud',
+    grid: 'app_book'
+)]
+#[SyliusCrudRoutes(
+    alias: 'app.book',
+    path: 'admin/bootstrap/books',
+    section: 'bootstrap',
+    redirect: 'update',
+    templates: 'bootstrap/book',
+    grid: 'app_book',
+)]
+class Book implements ResourceInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?String $title = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $authorName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthorName(): ?string
+    {
+        return $this->authorName;
+    }
+
+    public function setAuthorName(string $authorName): self
+    {
+        $this->authorName = $authorName;
+
+        return $this;
+    }
+}
---
+++ src/Grid/BookGrid.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Grid;
+
+use Sylius\Bundle\GridBundle\Builder\Action\CreateAction;
+use Sylius\Bundle\GridBundle\Builder\Action\DeleteAction;
+use Sylius\Bundle\GridBundle\Builder\Action\UpdateAction;
+use Sylius\Bundle\GridBundle\Builder\ActionGroup\ItemActionGroup;
+use Sylius\Bundle\GridBundle\Builder\ActionGroup\MainActionGroup;
+use Sylius\Bundle\GridBundle\Builder\Field\StringField;
+use Sylius\Bundle\GridBundle\Builder\Filter\StringFilter;
+use Sylius\Bundle\GridBundle\Builder\GridBuilderInterface;
+use Sylius\Bundle\GridBundle\Grid\AbstractGrid;
+use Sylius\Bundle\GridBundle\Grid\ResourceAwareGridInterface;
+use App\Entity\Book;
+
+final class BookGrid extends AbstractGrid implements ResourceAwareGridInterface
+{
+    public static function getName(): string
+    {
+        return 'app_book';
+    }
+
+    public function buildGrid(GridBuilderInterface $gridBuilder): void
+    {
+        $gridBuilder
+            ->addFilter(
+                StringFilter::create('search', ['title'])
+                    ->setLabel('sylius.ui.search')
+            )
+            ->addOrderBy('title', 'asc')
+            ->addField(
+                StringField::create('title')
+                    ->setLabel('Title')
+                    ->setSortable(true)
+            )
+            ->addField(
+                StringField::create('authorName')
+                    ->setLabel('Author')
+                    ->setSortable(true)
+            )
+            ->addActionGroup(
+                MainActionGroup::create(
+                    CreateAction::create(),
+                ),
+            )
+            ->addActionGroup(
+                ItemActionGroup::create(
+                    UpdateAction::create(),
+                    DeleteAction::create(),
+                ),
+            )
+        ;
+    }
+
+    public function getResourceClass(): string
+    {
+        return Book::class;
+    }
+}
