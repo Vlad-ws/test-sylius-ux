7c50d5ad55e85f402d07e7abe7ccb993dd0c3310
Loïc Frémont <lc.fremont@gmail.com>
2022-08-18 00:23:56 +0200

    test it

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -7,9 +7,35 @@ use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Resource\Action\PlaceHolderAction;
 use Sylius\Component\Resource\Annotation\SyliusCrudRoutes;
 use Sylius\Component\Resource\Annotation\SyliusRoute;
+use Sylius\Component\Resource\Doctrine\ORM\State\CollectionProvider;
+use Sylius\Component\Resource\Doctrine\ORM\State\ItemProvider;
 use Sylius\Component\Resource\Model\ResourceInterface;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
+#[SyliusRoute(
+    name: 'app_semantic_ui_book_create',
+    path: 'admin/semantic_ui/books/new',
+    methods: ['GET', 'POST'],
+    controller: PlaceHolderAction::class,
+    template: '@SyliusUxSemanticUi/crud/create.html.twig',
+    priority: 20,
+    section: 'semantic_ui',
+    resource: 'app.book',
+    operation: 'create',
+    provider: ItemProvider::class
+)]
+#[SyliusRoute(
+    name: 'app_semantic_ui_book_update',
+    path: 'admin/semantic_ui/books/{id}/edit',
+    methods: ['GET', 'PUT'],
+    controller: PlaceHolderAction::class,
+    template: '@SyliusUxSemanticUi/crud/update.html.twig',
+    priority: 20,
+    section: 'semantic_ui',
+    resource: 'app.book',
+    operation: 'update',
+    provider: ItemProvider::class
+)]
 #[SyliusCrudRoutes(
     alias: 'app.book',
     path: 'admin/semantic_ui/books',
@@ -17,13 +43,18 @@ use Sylius\Component\Resource\Model\ResourceInterface;
     redirect: 'update',
     templates: '@SyliusUxSemanticUi/crud',
     grid: 'app_book',
+    except: ['index', 'create', 'update'],
 )]
 #[SyliusRoute(
     name: 'app_semantic_ui_book_index',
     path: 'admin/semantic_ui/books',
-    section: 'semantic_ui',
-    operation: 'index',
     controller: PlaceHolderAction::class,
+    template: '@SyliusUxSemanticUi/crud/index.html.twig',
+    section: 'semantic_ui',
+    grid: 'app_book',
+    resource: 'app.book',
+    operation: 'index',
+    provider: CollectionProvider::class
 )]
 class Book implements ResourceInterface
 {
